cf631a7be7ceb53d22acd9393dbcae65802f72f9
jenish D J <117469634+Jenishdj@users.noreply.github.com>
2023-02-04 11:54:51 +0530

    youtube clone using Html,css and js

---
+++ js/script.js
@@ -0,0 +1,24 @@
+var menuicon = document.querySelector('.nav-left i');
+var sidebar =document.querySelector('.side-bar');
+var menuicon1 =document.querySelector('.menu-icon1');
+var offcanvas = document.querySelector('.offcanvas');
+var videos = document.querySelector('.videos');
+var sidebar3 = document.querySelector('.side-bar3');
+var menuicon3 = document.querySelector('.menu-icon3');
+
+
+
+
+menuicon.addEventListener('click',function(){
+  sidebar.classList.toggle('small-sidebar')
+})
+
+menuicon3.addEventListener('click',function(){
+  sidebar3.classList.toggle('small-sidebar3')
+})
+
+
+
+
+
+  
